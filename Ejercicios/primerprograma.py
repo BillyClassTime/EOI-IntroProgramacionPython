ba11398003d5022a01170a0b6201786694b39348
BillyClassTime <billy@billyclasstime.com>
2022-04-25 13:58:57 +0200

    Añadida documentación y ejercicios vistos en clase 25 abril

---
+++ Ejercicios/primerprograma.py
@@ -0,0 +1,20 @@
+# comentarios
+# Este es un primer programa en python
+
+textoUnalinea = "este texto se asigna en una linea"
+
+textdistintaslineas = """Escribir valores alfanumericos 
+       en distintas lineas"""
+
+# declaraciones de variables
+numero = 10
+Numero = 20
+saludo = "Hola mundo"
+print(numero)
+print(Numero)
+print(numero+Numero)
+print("saludos "+saludo)
+print(type(numero))
+print(type(saludo))
+#del numero 
+print(numero)
